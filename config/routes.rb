0083937491817172d20c657001b2555ccc1f246b
Henry Doan <henrydoan96@gmail.com>
2019-02-27 19:06:56 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    #controller  action
+  root 'subs#index'
+
+  resources :subs do
+    resources :topics
+  end
+
+  # /subs/:sub_id/topics/:id
+  # sub_id 7
+  # topic_id
+    # id 4
+
+  # /subs/7/topics/4
+
+  # resources :subs, only: [:index, :show]
+  # resources :subs, except: [:index, :show]
+  # get '/subs', to: 'subs#index'
+  # get '/subs/:id', to: 'subs#show'
+  # get '/subs/:id', to: 'subs#edit'
+  # put '/subs/:id', to: 'subs#update'
+  # delete '/subs/:id', to: 'subs#destroy'
+
+  # 7
+    # get '/subs/7', to: 'subs#show'
 end
